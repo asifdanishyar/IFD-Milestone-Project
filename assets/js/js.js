d06c79f30fc89d7c8cc0d59ad3ea73e01f3927d6
asifdanishyar <asifdanishyar@yahoo.com>
2020-04-26 17:30:11 +0000

    d

--- assets/js/js.js
+++ assets/js/js.js
@@ -16,3 +16,12 @@
                   $('nav').removeClass('black');
             }
       })
+
+//Image gallery
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}

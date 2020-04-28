4d70f2390a25193fd9363851338b65a49b9c711b
asifdanishyar <asifdanishyar@yahoo.com>
2020-04-28 09:28:18 +0000

    a

--- assets/js/js.js
+++ assets/js/js.js
@@ -50,4 +50,29 @@ for (var i = 0; i < menubtns.length; i++) {
        
        });
         }
-      
+      //send Email
+        
+function sendMail(contactForm) {
+    emailjs.send("gmail", "bamyanpage", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            contactForm.name.value="",
+            contactForm.emailaddress.value="",
+            contactForm.projectsummary.value=""
+            console.log("Successfully sent", response);
+           },
+          
+           
+        function(error) {
+            console.log("FAILED", error);
+            
+        }
+    
+    );
+    return false;  // To block from loading a new page
+    
+}

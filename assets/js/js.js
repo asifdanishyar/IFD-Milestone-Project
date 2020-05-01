845d9e2178c4dfe13433159a98279f83e19b8307
asifdanishyar <asifdanishyar@yahoo.com>
2020-05-01 10:51:45 +0000

    simi-final-version

--- assets/js/js.js
+++ assets/js/js.js
@@ -39,7 +39,7 @@ for (var i = 0; i < menubtns.length; i++) {
 
 //google map...................................................................................
 
-        function initMap(){
+       /* function initMap(){
             var options={
                 zoom: 12,
                 center: {lat: 34.81001, lng: 67.82121}
@@ -50,7 +50,7 @@ for (var i = 0; i < menubtns.length; i++) {
             map:marker
        
        });
-        }
+        } */ //e qesmata baz dobara pak kon....................
 
       //send Email.......................................................
         
